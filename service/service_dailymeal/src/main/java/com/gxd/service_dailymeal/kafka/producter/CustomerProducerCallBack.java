810016ff5eacd4353586d2acd463cf4f710b2497
Xuandong Guo <2427543166@qq.com>
2022-05-02 22:43:16 +0100

    KafKa

--- service/service_dailymeal/src/main/java/com/gxd/service_dailymeal/kafka/producter/CustomerProducerCallBack.java
+++ service/service_dailymeal/src/main/java/com/gxd/service_dailymeal/kafka/producter/CustomerProducerCallBack.java
@@ -14,6 +14,10 @@ public class CustomerProducerCallBack {
         // 指定KV的序列化类型, 调用org.apache.kafka.common.serialization方法;
         properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
         properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // 设置自己写的分区器
+        properties.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "com.gxd.service_dailymeal.kafka.producter");
+
         // 1. 生产者
         KafkaProducer kafkaProducer = new KafkaProducer<String,String>(properties);
         // 2. 发送数据，可以选择回调或者普通
